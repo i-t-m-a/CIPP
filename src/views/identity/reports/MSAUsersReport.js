3789d879342126df8a19e0b6dad083ab14e2b386
David Nauendorf <david.nauendorf@gmail.com>
2023-06-02 22:00:36 +0800

    msa report

--- src/views/identity/reports/MSAUsersReport.js
+++ src/views/identity/reports/MSAUsersReport.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { CButton } from '@coreui/react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { faEdit, faEllipsisV, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faEllipsisV, faEye, faobjectexclude } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { cellBooleanFormatter, CellTip } from 'src/components/tables'
 import { CippPageList } from 'src/components/layout'
@@ -20,11 +20,6 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
       <Link to={viewLink}>
         <CButton size="sm" variant="ghost" color="success">
           <FontAwesomeIcon icon={faEye} />
-        </CButton>
-      </Link>
-      <Link to={editLink}>
-        <CButton size="sm" variant="ghost" color="warning">
-          <FontAwesomeIcon icon={faEdit} />
         </CButton>
       </Link>
       <CButton size="sm" color="link" onClick={() => setOCVisible(true)}>
@@ -45,7 +40,6 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
           { label: 'City', value: `${row.city ?? ' '}` },
           { label: 'Department', value: `${row.department ?? ' '}` },
           { label: 'OnPrem Last Sync', value: `${row.onPremisesLastSyncDateTime ?? ' '}` },
-          { label: 'On Prem OU', value: `${row.onpremisesdistinguishedname ?? ' '}` },
           { label: 'Unique ID', value: `${row.id ?? ' '}` },
         ]}
         actions={[
@@ -56,140 +50,13 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             color: 'success',
           },
           {
-            icon: <FontAwesomeIcon icon={faEdit} className="me-2" />,
-            label: 'Edit User',
+            icon: <FontAwesomeIcon icon={faobjectexclude} className="me-2" />,
+            label: 'Exclude From MSA Report',
             link: editLink,
-            color: 'info',
-          },
-          {
-            label: 'Research Compromised Account',
-            link: `/identity/administration/ViewBec?userId=${row.id}&tenantDomain=${tenant.defaultDomainName}`,
-            color: 'info',
-          },
-          {
-            label: 'Create Temporary Access Password',
-            color: 'info',
+            color: 'danger',
             modal: true,
             modalUrl: `/api/ExecCreateTAP?TenantFilter=${tenant.defaultDomainName}&ID=${row.userPrincipalName}`,
             modalMessage: 'Are you sure you want to create a Temporary Access Pass?',
-          },
-          {
-            label: 'Rerequire MFA registration',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecResetMFA?TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to enable MFA for this user?',
-          },
-          {
-            label: 'Send MFA Push',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecSendPush?TenantFilter=${tenant.defaultDomainName}&UserEmail=${row.userPrincipalName}`,
-            modalMessage: 'Are you sure you want to send a MFA request?',
-          },
-          {
-            label: 'Convert to Shared Mailbox',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecConvertToSharedMailbox?TenantFilter=${tenant.defaultDomainName}&ID=${row.userPrincipalName}`,
-            modalMessage: 'Are you sure you want to convert this user to a shared mailbox?',
-          },
-          {
-            label: 'Enable Online Archive',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecEnableArchive?TenantFilter=${tenant.defaultDomainName}&ID=${row.userPrincipalName}`,
-            modalMessage: 'Are you sure you want to enable the online archive for this user?',
-          },
-          {
-            label: 'Set Out of Office',
-            color: 'info',
-            modal: true,
-            modalType: 'POST',
-            modalBody: {
-              user: row.userPrincipalName,
-              TenantFilter: tenant.defaultDomainName,
-              message: row.message,
-            },
-            modalUrl: `/api/ExecSetOoO`,
-            modalInput: true,
-            modalMessage:
-              'Enter a out of office message and press continue to set the out of office.',
-          },
-          {
-            label: 'Disable Out of Office',
-            color: 'info',
-            modal: true,
-            modalType: 'POST',
-            modalBody: {
-              user: row.userPrincipalName,
-              TenantFilter: tenant.defaultDomainName,
-              Disable: true,
-            },
-            modalUrl: `/api/ExecSetOoO`,
-            modalMessage: 'Are you sure you want to disable the out of office?',
-          },
-          {
-            label: 'Disable Email Forwarding',
-            color: 'info',
-            modal: true,
-            modalType: 'POST',
-            modalBody: {
-              user: row.userPrincipalName,
-              TenantFilter: tenant.defaultDomainName,
-              message: row.message,
-            },
-            modalUrl: `/api/ExecDisableEmailForward`,
-            modalMessage: 'Are you sure you want to disable forwarding of this users emails?',
-          },
-          {
-            label: 'Block Sign In',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecDisableUser?TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to block the sign in for this user?',
-          },
-          {
-            label: 'Unblock Sign In',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecDisableUser?Enable=true&TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to enable this user?',
-          },
-          {
-            label: 'Reset Password (Must Change)',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecResetPass?MustChange=true&TenantFilter=${tenant.defaultDomainName}&ID=${row.id}&displayName=${row.displayName}`,
-            modalMessage: 'Are you sure you want to reset the password for this user?',
-          },
-          {
-            label: 'Reset Password',
-            color: 'info',
-            modal: true,
-            modalUrl: `/api/ExecResetPass?MustChange=false&TenantFilter=${tenant.defaultDomainName}&ID=${row.id}&displayName=${row.displayName}`,
-            modalMessage: 'Are you sure you want to reset the password for this user?',
-          },
-          {
-            label: 'Clear ImmutableId',
-            color: 'warning',
-            modal: true,
-            modalUrl: `/api/ExecClrImmId?TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to clear the ImmutableId for this user?',
-          },
-          {
-            label: 'Revoke all user sessions',
-            color: 'danger',
-            modal: true,
-            modalUrl: `/api/ExecRevokeSessions?TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to revoke this users sessions?',
-          },
-          {
-            label: 'Delete User',
-            color: 'danger',
-            modal: true,
-            modalUrl: `/api/RemoveUser?TenantFilter=${tenant.defaultDomainName}&ID=${row.id}`,
-            modalMessage: 'Are you sure you want to delete this user?',
           },
         ]}
         placement="end"
@@ -235,13 +102,6 @@ const Users = (row) => {
       cell: (row) => CellTip(row['mail']),
       exportSelector: 'mail',
       minWidth: '250px',
-    },
-    {
-      name: 'On Prem OU',
-      selector: (row) => row['onpremisesdistinguishedname'],
-      sortable: true,
-      exportSelector: 'onpremisesdistinguishedname',
-      minWidth: '200px',
     },
     {
       name: 'Licenses',
